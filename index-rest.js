bade87d166f2a7a99205c2e65fb7e597520d8758
Davi Camarinha <davi@gavea.com>
2023-08-09 16:24:16 -0300

    add cache function

--- index-rest.js
+++ index-rest.js
@@ -1,6 +1,10 @@
 const express = require("express");
+
 const https = require("node:https");
+
 const dotenv = require("dotenv");
+const cache = require("memory-cache");
+
 dotenv.config();
 
 const app = express();
@@ -9,7 +13,25 @@ const port = 3000;
 const key = process.env.KEY;
 const baseUrlCurrent = "https://api.weatherapi.com/v1/current.json?q=";
 
-app.get("/currentWeather/", async (req, res) => {
+const cacheMiddleware = (duration) => {
+  return (req, res, next) => {
+    const key = "__express__" + req.originalUrl || req.url;
+    const cachedBody = cache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        cache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
+
+app.get("/currentWeather/", cacheMiddleware(180), async (req, res) => {
   console.log("searching data to: " + req.query.cityName);
 
   await https
